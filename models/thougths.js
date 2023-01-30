a3765a904e6a6782672073d2544fb436431be7e8
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-01-29 19:33:58 -0500

    filled in thoughts.js model, need to create modle for reactions as well as develp datehelper in utils folder

--- models/thougths.js
+++ models/thougths.js
@@ -1,21 +1,41 @@
 const { Schema, Types } = require("mongoose");
- 
+const reactionSchema = require("./Reaction");
+const dateFormat = require("../utils/dateHelper");
 
 const ThoughtSchema = new Schema(
     {
-      thoughtText: {
-        type: String,
-        required: "Thought is Required",
-        minlength: 1,
-        maxlength: 280,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        //  get method to format the timestamp
-        get: (timestamp) => dateFormat(timestamp),
-      },
-      username: {
-        type: String,
-        required: true,
-      },
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //  get method to format the timestamp
+            get: (timestamp) => dateFormat(timestamp),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJson: {
+            virtuals: true,
+        },
+        id:false
+ 
+    }
+);
+thoughtSchema
+.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', ThoughtSchema)
+
+module.exports = Thought;
