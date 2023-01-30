1a0e614820942e6b777692441815d04a557a4c26
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-01-30 18:18:26 -0500

    updated index.js in models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const users = require("./users");
+const Thought = require('./thougths');
+const reactionSchema = require('./Reaction');
 
-
-module.exports = {users,}
+module.exports = {users,Thought,reactionSchema}
